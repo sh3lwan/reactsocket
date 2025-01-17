bcb81c992c0931efc7f253f780d5f398fb6c1bb4
Shaban Olwan <sh.3ban@live.com>
2025-01-17 02:28:27 +0200

    fix deploy script + env

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,11 @@ cp nginx/nginx.before.conf nginx/nginx.conf
 docker compose up -d nginx
 
 # Wait until nginx is up and healthy
-echo "Waiting for nginx to be ready..."
-while ! docker inspect --format='{{.State.Health.Status}}' $(docker ps -q --filter "name=nginx") | grep -q "healthy"; do
-    echo "Nginx is not ready yet. Retrying in 2 seconds..."
-    sleep 2
-done
+#echo "Waiting for nginx to be ready..."
+#while ! docker inspect --format='{{.State.Health.Status}}' $(docker ps -q --filter "name=nginx") | grep -q "healthy"; do
+#    echo "Nginx is not ready yet. Retrying in 2 seconds..."
+#    sleep 2
+#done
 
 # step 2: run certbot to obtain ssl certificate
 echo "requesting ssl certificate..."
@@ -29,4 +29,4 @@ echo "switching to ssl-enabled config..."
 cp nginx/nginx.after.conf nginx/nginx.conf
 
 # switch to ssl-enabled configuration
-docker comopse down && docker compose up --build -d --remove-orphans
+docker compose down && docker compose up --build -d --remove-orphans
